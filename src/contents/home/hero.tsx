f31df9688d16732153afece811bfa35c7b92084c
pitokai <crisvitoc@gmail.com>
2022-09-13 18:35:12 +0800

    feat: set font for app and hero

--- src/contents/home/hero.tsx
+++ src/contents/home/hero.tsx
@@ -2,7 +2,7 @@ export function HomeHero() {
   return (
     <div className="bg-home-hero flex h-[420px] items-center justify-center bg-slate-600">
       <div className="text-center">
-        <h1 className="text-7xl font-bold uppercase leading-tight decoration-4 hover:underline">
+        <h1 className="font-brand-bold text-7xl uppercase leading-tight tracking-widest">
           Frigga <br /> Survival <br /> Fest
         </h1>
       </div>
