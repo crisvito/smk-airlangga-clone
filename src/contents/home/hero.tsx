f866b80f227725a15e4369e7b48a58c630325d61
pitokai <crisvitoc@gmail.com>
2022-09-13 18:23:22 +0800

    feat: embed google fonts

--- src/contents/home/hero.tsx
+++ src/contents/home/hero.tsx
@@ -2,8 +2,8 @@ export function HomeHero() {
   return (
     <div className="bg-home-hero flex h-[420px] items-center justify-center bg-slate-600">
       <div className="text-center">
-        <h1 className="text-7xl uppercase leading-tight">
-          Frigga <br /> Survivor <br /> Fest
+        <h1 className="text-7xl font-bold uppercase leading-tight decoration-4 hover:underline">
+          Frigga <br /> Survival <br /> Fest
         </h1>
       </div>
     </div>
