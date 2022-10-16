06fb83c61875dec1620cc44d4e8286ab1163f4e1
pitokai <crisvitoc@gmail.com>
2022-10-16 17:09:55 +0800

    feat: add auto scroll to top

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,14 +1,33 @@
+import { ArrowCircleUp } from "phosphor-react";
+import { useEffect, useState } from "react";
 import { Footer, NavigationBar } from "../components";
 import { HomeHero, HomeEvent } from "../contents";
 import { AboutHero } from "../contents";
 
 export function RouteHome() {
+  const [up, setUp] = useState(false);
+
+  window.addEventListener("scroll", function () {
+    window.scrollY >= 100 ? setUp(true) : setUp(false);
+  });
+  function handleUp() {
+    window.scrollTo(0, 0);
+  }
+
   return (
     <div className="flex-[1]">
       <NavigationBar />
       <HomeHero />
       <HomeEvent />
       <AboutHero />
+      <div className="fixed bottom-5 right-5">
+        <ArrowCircleUp
+          size={54}
+          weight="fill"
+          className={`text-black dark:text-white ${up ? "" : "hidden"}`}
+          onClick={() => handleUp()}
+        />
+      </div>
       <Footer />
     </div>
   );
